20cd92cef20257523901195ba563e35e434c3ba4
pedroarthuror31 <pedroarthuror2005@gmail.com>
2026-06-21 20:09:52 -0300

    Create index.php

---
+++ codigos/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema de Gerenciamento Acadêmico</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+
+    <h1>Sistema de Gerenciamento Acadêmico</h1>
+
+    <p>Escolha uma opção:</p>
+
+    <a href="alunos.php">
+        <button>Gerenciar Alunos</button>
+    </a>
+
+    <a href="atividades.php">
+        <button>Gerenciar Atividades</button>
+    </a>
+
+</div>
+
+</body>
+</html>
