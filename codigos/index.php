79cfc503ce222db5ef7243caa5e722f55e2fe8f2
pedroarthuror31 <pedroarthuror2005@gmail.com>
2026-07-03 18:26:44 -0300

    Update index.php

--- codigos/index.php
+++ codigos/index.php
@@ -2,9 +2,11 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Gerenciamento Acadêmico</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
 <div class="container">
@@ -17,8 +19,28 @@
         <button>Gerenciar Alunos</button>
     </a>
 
+    <br><br>
+
     <a href="atividades.php">
         <button>Gerenciar Atividades</button>
+    </a>
+
+    <br><br>
+
+    <a href="entregas.php">
+        <button>Gerenciar Entregas</button>
+    </a>
+
+    <br><br>
+
+    <a href="consultas.php">
+        <button>Consultar Notas por Atividade</button>
+    </a>
+
+    <br><br>
+
+    <a href="alunos_sem_atividade.php">
+        <button>Alunos sem Atividades</button>
     </a>
 
 </div>
