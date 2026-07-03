9d4c82b016eccb524500f8c1833fb02dbc9231c2
pedroarthuror31 <pedroarthuror2005@gmail.com>
2026-07-03 18:18:21 -0300

    Update insert.sql

--- codigos/insert.sql
+++ codigos/insert.sql
@@ -1,9 +1,21 @@
+USE trabalho_bd;
+
+-- Inserção de alunos
 INSERT INTO alunos (nome, email, curso, data_nascimento)
 VALUES
 ('Pedro Arthur', 'pedro@email.com', 'Engenharia de Computação', '2005-05-31'),
 ('Bruna Kamille', 'bruna@email.com', 'Engenharia de Computação', '2005-03-28');
 
-INSERT INTO atividades (titulo, descricao, nota, data_entrega, id_aluno)
+-- Inserção de atividades
+INSERT INTO atividades
+(nome_atividade, professor_responsavel, valor, data_prevista)
 VALUES
-('Trabalho de Banco de Dados', 'Aplicação web com MySQL', 9.5, '2026-07-01', 1),
-('Lista de Programação', 'Exercícios em Python', 8.0, '2026-06-28', 2);
+('Trabalho de Banco de Dados', 'Prof. Marcelo', 10.00, '2026-07-01'),
+('Lista de Programação', 'Prof. Alexandre', 10.00, '2026-06-28');
+
+-- Relação entre alunos e atividades
+INSERT INTO aluno_atividade
+(id_aluno, id_atividade, data_entrega, nota)
+VALUES
+(1, 1, '2026-07-01', 9.5),
+(2, 2, '2026-06-28', 8.0);
