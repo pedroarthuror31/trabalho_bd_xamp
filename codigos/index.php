9e9906365bbb84e9e68f50e6aa299b187d01cbb7
pedroarthuror31 <pedroarthuror2005@gmail.com>
2026-07-05 21:29:30 -0300

    Update index.php

--- codigos/index.php
+++ codigos/index.php
@@ -2,7 +2,6 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Gerenciamento Acadêmico</title>
     <link rel="stylesheet" href="style.css">
 </head>
@@ -11,39 +10,50 @@
 
 <div class="container">
 
-    <h1>Sistema de Gerenciamento Acadêmico</h1>
+<h1>Sistema de Gerenciamento Acadêmico</h1>
 
-    <p>Escolha uma opção:</p>
+<p>Escolha uma opção:</p>
 
-    <a href="alunos.php">
-        <button>Gerenciar Alunos</button>
-    </a>
+<h2>Gerenciamento</h2>
 
-    <br><br>
+<a href="alunos.php">
+    <button>Gerenciar Alunos</button>
+</a>
 
-    <a href="atividades.php">
-        <button>Gerenciar Atividades</button>
-    </a>
+<a href="atividades.php">
+    <button>Gerenciar Atividades</button>
+</a>
 
-    <br><br>
+<a href="entregas.php">
+    <button>Gerenciar Entregas</button>
+</a>
 
-    <a href="entregas.php">
-        <button>Gerenciar Entregas</button>
-    </a>
+<hr>
 
-    <br><br>
+<h2>Consultas SQL</h2>
 
-    <a href="consultas.php">
-        <button>Consultar Notas por Atividade</button>
-    </a>
+<a href="consultas.php">
+    <button>Notas por Atividade (INNER JOIN)</button>
+</a>
 
-    <br><br>
+<a href="relatorio_atividades.php">
+    <button>Relatório de Atividades (COUNT + AVG)</button>
+</a>
 
-    <a href="alunos_sem_atividade.php">
-        <button>Alunos sem Atividades</button>
-    </a>
+<a href="alunos_aprovados.php">
+    <button>Alunos Aprovados (ANY)</button>
+</a>
+
+<a href="alunos_com_atividades.php">
+    <button>Alunos com Atividades (EXISTS)</button>
+</a>
+
+<a href="alunos_sem_atividade.php">
+    <button>Alunos sem Atividades (NOT IN)</button>
+</a>
 
 </div>
 
 </body>
+</html>
 </html>
